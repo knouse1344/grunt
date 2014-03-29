1ac1bb360d993b2d690f552bc67f6900f78736a7
Duri Chitayat <duri@durichitayat.net>
2014-03-29 12:44:29 -0400

    - include "Has Feedback"

--- wp-content/themes/nitro/searchform.php
+++ wp-content/themes/nitro/searchform.php
@@ -1,7 +1,7 @@
 <?php ?>
 
 <form role="search" method="get" class="search-form form-inline" action="<?php echo home_url( '/' ); ?>">
-	<div class="form-group">
+	<div class="form-group has-feedback">
 		<input type="text" class="form-control" id="" placeholder="Search …" value="" name="s" title="Search">
         <span class="glyphicon glyphicon-search form-control-feedback"></span>
   </div>
