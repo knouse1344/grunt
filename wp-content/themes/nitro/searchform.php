c9099f34b99b8703534e4602b96f0936a65c56d0
Duri Chitayat <duri@durichitayat.net>
2014-03-29 12:41:43 -0400

    Update search form

--- wp-content/themes/nitro/searchform.php
+++ wp-content/themes/nitro/searchform.php
@@ -1,20 +1,12 @@
 <?php ?>
 
 <form role="search" method="get" class="search-form form-inline" action="<?php echo home_url( '/' ); ?>">
-	<div class="form-group has-success has-feedback">
+	<div class="form-group">
 		<input type="text" class="form-control" id="" placeholder="Search …" value="" name="s" title="Search">
         <span class="glyphicon glyphicon-search form-control-feedback"></span>
   </div>
-  <!--<input type="submit" class="search-submit" value="Search" />-->
 </form>
 
 
-<form class="form-inline" role="form">
-  <div class="form-group has-success has-feedback">
-    <label class="control-label" for="inputSuccess4">Input with success</label>
-    <input type="text" class="form-control" id="inputSuccess4">
-    <span class="glyphicon glyphicon-ok form-control-feedback"></span>
-  </div>
-</form>
 
 <?php ?>
