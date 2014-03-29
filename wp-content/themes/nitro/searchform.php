e0673e753f767caf7c4a164556719169a20ca821
Duri Chitayat <duri@durichitayat.net>
2014-03-29 14:42:45 -0400

    Update Search Form Width

--- wp-content/themes/nitro/searchform.php
+++ wp-content/themes/nitro/searchform.php
@@ -2,7 +2,7 @@
 
 <form role="search" method="get" class="search-form form-inline navbar-right" action="<?php echo home_url( '/' ); ?>">
 	<div class="form-group has-feedback">
-		<input type="text" class="form-control" id="" placeholder="Search …" value="" name="s" title="Search">
+		<input type="text" class="form-control" id="search-input" placeholder="Search …" value="" name="s" title="Search">
         <span class="glyphicon glyphicon-search form-control-feedback"></span>
   </div>
 </form>
