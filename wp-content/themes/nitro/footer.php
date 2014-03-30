2d8060e6032c5446aa946c80bfc2df6b92b92607
Duri Chitayat <duri@durichitayat.net>
2014-03-30 17:25:13 -0400

    footer color

--- wp-content/themes/nitro/footer.php
+++ wp-content/themes/nitro/footer.php
@@ -1,27 +1,25 @@
 </div><!-- END WRAP-->
 
-    <div id="footer" class="affix-bottom">
+    <div id="footer" class="affix-bottom small">
       <div class="container">
 		<div class="row">
-           
-            <div class="col-sm-12 col-xs-12">
-                <div class="col-sm-3 col-xs-12">
-                    <?php dynamic_sidebar( 'footer1' ); ?>
-                </div>
-                <div class="col-sm-3 col-xs-12">
-                    <?php dynamic_sidebar( 'footer2' ); ?>
-                </div>
-                <div class="col-sm-3 col-xs-12">
-                    <?php dynamic_sidebar( 'footer3' ); ?>
-                </div>
-                <div class="col-sm-3 col-xs-12">
-                    <?php dynamic_sidebar( 'footer4' ); ?>
-                </div>
+            <div class="col-sm-3 col-xs-12">
+                <?php dynamic_sidebar( 'footer1' ); ?>
             </div>
-            <div class="col-sm-12 col-xs-12">
-                <p class="">Copyright <span class="glyphicon glyphicon-copyright-mark"></span> 2014 Net@Work</p>
+            <div class="col-sm-3 col-xs-12">
+                <?php dynamic_sidebar( 'footer2' ); ?>
             </div>
-
+            <div class="col-sm-3 col-xs-12">
+                <?php dynamic_sidebar( 'footer3' ); ?>
+            </div>
+            <div class="col-sm-3 col-xs-12">
+                <?php dynamic_sidebar( 'footer4' ); ?>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12 col-xs-12">
+                <p class="text-right">Copyright <span class="glyphicon glyphicon-copyright-mark"></span> 2014 Net@Work</p>
+            </div>
         </div>
       </div>
     </div>
