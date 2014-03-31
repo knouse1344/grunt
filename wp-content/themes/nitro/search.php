182ecb9cad11e7b3635b4d666a4ca49fce370b62
Duri Chitayat <duri@durichitayat.net>
2014-03-31 01:18:19 -0400

    Removed Function

--- wp-content/themes/nitro/search.php
+++ wp-content/themes/nitro/search.php
@@ -12,8 +12,6 @@
                  
                     <header>
                         <h2><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'typical' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-            
-                        <?php typical_posted_on(); ?>
                     </header>
             
                         <?php the_excerpt(); ?>
