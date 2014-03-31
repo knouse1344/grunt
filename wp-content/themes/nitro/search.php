b7587bef573c3fdf7c6e1bdf79654acaabde4515
Duri Chitayat <duri@durichitayat.net>
2014-03-31 01:30:26 -0400

    Update Search Page with Media Body

--- wp-content/themes/nitro/search.php
+++ wp-content/themes/nitro/search.php
@@ -8,17 +8,13 @@
             
             <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
             
-                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                 
-                    <header>
-                        <h2><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'typical' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-                    </header>
+                <div class="media">
+                    <div class="media-body">
+                        <h4 class="media-heading"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'typical' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h4>
             
                         <?php the_excerpt(); ?>
-            
-                    <footer>
-                        <p>
-                            <?php if ( count( get_the_category() ) ) : ?>
+                    
+                        <p><?php if ( count( get_the_category() ) ) : ?>
                                     <?php printf( __( 'Posted in %2$s', 'typical' ), 'entry-utility-prep entry-utility-prep-cat-links', get_the_category_list( ', ' ) ); ?> <span aria-hidden="true">&#x2767;</span>
                             <?php endif; ?>
                             <?php
@@ -27,11 +23,9 @@
                             ?>
                                     <?php printf( __( 'Tagged %2$s', 'typical' ), 'entry-utility-prep entry-utility-prep-tag-links', $tags_list ); ?> <span aria-hidden="true">&#x2767;</span>
                             <?php endif; ?>
-                            <?php comments_popup_link( __( 'Leave a comment', 'typical' ), __( '1 Comment', 'typical' ), __( '% Comments', 'typical' ) ); ?>
-                            <?php edit_post_link( __( 'Edit', 'typical' ), '<span aria-hidden="true">&#x2767;</span> ', '' ); ?>
                         </p>
-                    </footer>
-                </article>
+                    </div>
+               </div>
                 
             <?php endwhile; // end of the loop. ?>
             
