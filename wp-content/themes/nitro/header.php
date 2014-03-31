4ba76cb2f72e243919194aa5fa0826526e27c519
Duri Chitayat <duri@durichitayat.net>
2014-03-30 23:35:09 -0400

    Update Page Title CSS

--- wp-content/themes/nitro/header.php
+++ wp-content/themes/nitro/header.php
@@ -130,7 +130,7 @@
                 <div class="container">
                 <div class="row">
                     <div class="col-xs-12">
-                        <p><span class="icon icon-home"></span> I'm a Page.</p>
+                        <span class="icon icon-home"></span><h1>Page Title</h1>
                     </div>
                 </div>
                 </div>
