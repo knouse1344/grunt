bc0526fee35f8bd0608888ee199545f473e10f55
Duri Chitayat <duri@durichitayat.net>
2014-04-09 00:09:53 -0400

    update 404

--- wp-content/themes/nitro/404.php
+++ wp-content/themes/nitro/404.php
@@ -3,7 +3,7 @@
     <div class="row">
 		
 		<!--- Start Body Content --->
-		<div id="primary-content fourohfour" class="col-xs-12" role="main">                    
+		<div id="fourohfour" class="col-xs-12" role="main">                    
             <h1>404</h1>
             <h2>I'm sorry, could we help you find what you were looking for?</h2>
             <p><?php get_search_form(); ?></p>
