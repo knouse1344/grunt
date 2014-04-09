4f6b41cc1884035d44927a3198c5732a73ac8d9b
Duri Chitayat <duri@durichitayat.net>
2014-04-08 23:25:40 -0400

    Update 404

--- wp-content/themes/nitro/404.php
+++ wp-content/themes/nitro/404.php
@@ -1,20 +1,13 @@
 <?php get_header(); ?>
-
-  <div class="container" role="main">
-      <div class="row">
+<div class="container" role="main">
+    <div class="row">
 		
 		<!--- Start Body Content --->
-		<div id="primary-content" class="col-sm-9 col-xs-12" role="main">
-			<div class="row">
-				<div class="col-sm-12 col-xs-12" id="fourohfour">
-                    
-                    <h1>404</h1>
-                    <h2>Not Found</h2>
-                    
-                </div>
-            </div>    
-		</div>
-	</div>     
-  </div>
-
+		<div id="primary-content fourohfour" class="col-xs-12" role="main">                    
+            <h1>404</h1>
+            <h2>Not Found</h2>       
+        </div>
+          
+    </div>    
+</div>
 <?php get_footer(); ?>
