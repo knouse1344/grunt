eecabbd79f3f7ac78e94dc181a5592859b1e24f4
Duri Chitayat <duri@durichitayat.net>
2014-04-08 22:59:57 -0400

    Add 404

---
+++ wp-content/themes/nitro/404.php
@@ -0,0 +1,20 @@
+<?php get_header(); ?>
+
+  <div class="container" role="main">
+      <div class="row">
+		
+		<!--- Start Body Content --->
+		<div id="primary-content" class="col-sm-9 col-xs-12" role="main">
+			<div class="row">
+				<div class="col-sm-12 col-xs-12" id="404">
+                    
+                    <h1>404</h1>
+                    <h2>Not Found</h2>
+                    
+                </div>
+            </div>    
+		</div>
+	</div>     
+  </div>
+
+<?php get_footer(); ?>
