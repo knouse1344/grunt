ca4eb5444eef12054cf157467386e09d25201b95
Duri Chitayat <duri@durichitayat.net>
2014-04-09 23:32:13 -0400

    Update Portfolio

--- wp-content/themes/nitro/archive-portfolio.php
+++ wp-content/themes/nitro/archive-portfolio.php
@@ -4,7 +4,7 @@
       <div class="row">
 		
 		<!--- Start Body Content --->
-		<div id="primary-content" class="col-sm-9 col-xs-12">
+		<div id="primary-content" class="col-xs-12">
 			<div class="row">
 				<div class="col-sm-12 col-xs-12">
 					<?php
@@ -17,25 +17,23 @@
 					{
 					 while ( $loop->have_posts() ) : $loop->the_post(); ?>
 					<div class="col-sm-4 col-xs-12">
-						<div class="thumbnail">
+						
 							<?php if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
-							$thumb = the_post_thumbnail(array(300,220));
-							}  
+							$thumb = the_post_thumbnail(array(350,220)); }  
 							$naw_url = get_post_meta( $post->ID, '_cd_naw_url', true );
-							if ('' != $naw_url)
-							{
-								$siteurl = '<a href="' .$naw_url. '" target="_blank" class="btn btn-info btn-xs" role="button">Visit Site </a>';
-							}
-							else
-							{
-								$siteurl = '';
-							}
-							?>
-							<img data-src="<?php echo $thumb; ?>">
+							if ('' != $naw_url){
+								$siteurl = '<a href="' .$naw_url. '" target="_blank">'; }
+							else {
+								$siteurl = ''; } 
+                                    
+                                    echo '<a href="' .$naw_url. '">'; 
+                                    echo $thumb; 
+                                    echo '</a>'?><!--whats this?-->
+                    
 						  <div class="caption">
 							<h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
 							<p><?php the_excerpt(); ?></p>
-							<p><a href="<?php the_permalink(); ?>" class="btn btn-primary btn-xs" role="button">Read More!</a> &nbsp; <?php echo $siteurl; ?></p>
+							<p><a href="<?php the_permalink(); ?>" class="" role="button">Read More!</a></p>
 						  </div>
 						</div>
 					</div>
@@ -49,23 +47,7 @@
 				</div>
 			</div>
 		</div>
-    	<!-- Right Sidebar -->
-		<div class="col-sm-3 col-xs-12" id="sidebar">
-				
-			<div class="panel panel-default">
-			  <div class="panel-heading"><b>Contact Us</b></div>
-			  <div class="panel-body">
-				<b>Call Us:</b> 1-800-719-3307<br/>
-				<b>Email:</b> websales@netatwork.com
-			  </div>
-			</div>
-			<div class="panel panel-default">
-			  <div class="panel-heading"><b>The Net@Work Advantage</b></div>
-			  <div class="panel-body">
-				Net@Work brings the combined resources and talent of its 150+ designers, developers, business analysts and network engineers to ensure that your website can integrate with your entire business process. As Computer Reseller News stated in its August 2006 issue, Net@Work is the “new Super VAR,” providing its clients with “end-to-end IT solutions - from business applications and e-commerce solutions to custom application development to network infrastructure.”
-			  </div>
-			</div>
-		</div>
+
 	</div>     
   </div>
 
